6c71503d3f7a5b2676f85af49e3ddeb8da07aeff
sauravgyawali88 <gyawalisaurav892@gmail.com>
2025-06-18 22:18:28 -0400

    edited about me section

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
 */
 //
-// Scripts
+// Scripts 
 // 
 
 window.addEventListener('DOMContentLoaded', event => {
